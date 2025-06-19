46961d205a86c217e8dec1f8ae285779b3b7b203
Andrei <andrei.i@posteo.de>
2025-06-19 00:00:00 +0000

    Use RGS v2 endpoint

--- libs/sdk-core/src/ldk/config.rs
+++ libs/sdk-core/src/ldk/config.rs
@@ -8,7 +8,7 @@ impl Config {
     pub fn mainnet() -> Self {
         Self {
             esplora_url: "https://blockstream.info/api".to_string(),
-            rgs_url: "https://rapidsync.lightningdevkit.org/snapshot".to_string(),
+            rgs_url: "https://rapidsync.lightningdevkit.org/snapshot/v2".to_string(),
             vss_url: "http://localhost:4080/vss".to_string(),
         }
     }
@@ -16,7 +16,7 @@ impl Config {
     pub fn regtest() -> Self {
         Self {
             esplora_url: "http://localhost:30000".to_string(),
-            rgs_url: "http://localhost:8011".to_string(),
+            rgs_url: "http://localhost:8011/v2".to_string(),
             vss_url: "http://localhost:3080/vss".to_string(),
         }
     }
